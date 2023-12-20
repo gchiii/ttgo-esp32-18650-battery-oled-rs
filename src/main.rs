21d3290fc82db5b67668ba9e4db5f9c5238bf18d
George C. Huntington III <ghuntington@hubbell.com>
2023-12-20 17:49:00 -0500

    add some proper error checks

--- src/main.rs
+++ src/main.rs
@@ -19,24 +19,49 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    let peripherals = Peripherals::take().unwrap();
+    let peripherals = match Peripherals::take() {
+        Ok(p) => p,
+        Err(e) => {
+            log::error!("error {}", e);
+            return;
+        },
+    };
+
     let i2c = peripherals.i2c0;
     let sda = peripherals.pins.gpio5;
     let scl = peripherals.pins.gpio4;
 
-    let mut led_pin = PinDriver::output_od(peripherals.pins.gpio16).unwrap();
+    let mut led_pin = match PinDriver::output_od(peripherals.pins.gpio16) {
+        Ok(l) => l,
+        Err(e) => {
+            log::error!("error {}", e);
+            return;
+        },
+    };
 
     if let Err(e) = led_pin.set_high() {
         log::error!("error {}", e);
     }
     let config = I2cConfig::new().baudrate(400.kHz().into());
-    let i2c = I2cDriver::new(i2c, sda, scl, &config).unwrap();
-    let mut interface = I2CDisplayInterface::new(i2c);
+    let i2c = match I2cDriver::new(i2c, sda, scl, &config) {
+        Ok(i2c) => i2c,
+        Err(e) => {
+            log::error!("error {}", e);
+            return;
+        },
+    };
+    let interface = I2CDisplayInterface::new(i2c);
     let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
         .into_terminal_mode();
 
-    display.init().unwrap();
-    write!(display, "Hello, rust!").unwrap();
+    if let Err(e) = display.init() {
+        log::error!("error {}", e);
+        return;
+    }
+    if let Err(e) = write!(display, "Hello, rust!") {
+        log::error!("error {}", e);
+        return;
+    }
 
     log::info!("Hello, world!");
     loop {
